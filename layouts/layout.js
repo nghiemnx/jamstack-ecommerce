93e8978f2b1b3087f203333c550e69c54989857b
nghiem.nguyen <nghiem.nguyen@kyanon.digital>
2022-06-05 23:27:25 +0700

    [Update] sticky completed

--- layouts/layout.js
+++ layouts/layout.js
@@ -1,8 +1,8 @@
-import Link from 'next/link'
-import { slugify } from '../utils/helpers'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify'
-import { navItemLength } from '../ecommerce.config'
+import Link from "next/link"
+import { slugify } from "../utils/helpers"
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer } from "react-toastify"
+import { navItemLength } from "../ecommerce.config"
 
 export default function Layout({ children, categories }) {
   if (categories.length > navItemLength) {
@@ -10,12 +10,22 @@ export default function Layout({ children, categories }) {
   }
   return (
     <div>
-      <nav>
+      <nav
+        style={{
+          width: "100%",
+          position: "fixed",
+          top: 0,
+          backgroundColor: "#F3E9DC",
+          zIndex: 999
+        }}
+      >
         <div className="flex justify-center">
-          <div className="
+          <div
+            className="
             mobile:px-12 sm:flex-row sm:pt-12 sm:pb-6 desktop:px-0
             px-4 pt-8 flex flex-col w-fw
-          ">
+          "
+          >
             <div className="mb-4 sm:mr-16 max-w-48 sm:max-w-none">
               <Link href="/">
                 <a aria-label="Home">
@@ -26,38 +36,39 @@ export default function Layout({ children, categories }) {
             <div className="flex flex-wrap mt-1">
               <Link href="/">
                 <a aria-label="Home">
-                  <p className="
+                  <p
+                    className="
                     sm:mr-8 sm:mb-0
                     mb-4 text-left text-smaller mr-4
-                  ">
-                  Home
+                  "
+                  >
+                    Home
                   </p>
                 </a>
               </Link>
-              {
-                categories.map((category, index) => (
-                  <Link
-                    href={`/category/${slugify(category)}`}
-                    key={index}
-                  >
-                    <a aria-label={category}>
-                      <p className="
+              {categories.map((category, index) => (
+                <Link href={`/category/${slugify(category)}`} key={index}>
+                  <a aria-label={category}>
+                    <p
+                      className="
                           sm:mr-8 sm:mb-0
                           mb-4 text-left text-smaller mr-4
-                        ">
-                        {category.charAt(0).toUpperCase() + category.slice(1)}
-                      </p>
-                    </a>
-                  </Link>
-                ))
-              }
+                        "
+                    >
+                      {category.charAt(0).toUpperCase() + category.slice(1)}
+                    </p>
+                  </a>
+                </Link>
+              ))}
               <Link href="/categories">
                 <a aria-label="All categories">
-                  <p className="
+                  <p
+                    className="
                     sm:mr-8 sm:mb-0
                     mb-4 text-left text-smaller mr-4 
-                  ">
-                  All
+                  "
+                  >
+                    All
                   </p>
                 </a>
               </Link>
@@ -65,25 +76,34 @@ export default function Layout({ children, categories }) {
           </div>
         </div>
       </nav>
-      <div className="mobile:px-10 px-4 pb-10 flex justify-center">
+      <div
+        style={{ paddingTop: 100 }}
+        className="mobile:px-10 px-4 pb-10 flex justify-center"
+      >
         <main className="w-fw">{children}</main>
       </div>
       <footer className="flex justify-center">
-        <div className="
+        <div
+          className="
         sm:flex-row sm:items-center
         flex-col
         flex w-fw px-12 py-8
         desktop:px-0
         border-solid
-        border-t border-gray-300">
-          <span className="block text-gray-700 text-xs">Copyright © 2021 JAMstack Ecommerce. All rights reserved.</span>
-          <div className="
+        border-t border-gray-300"
+        >
+          <span className="block text-gray-700 text-xs">
+            Copyright © 2021 JAMstack Ecommerce. All rights reserved.
+          </span>
+          <div
+            className="
             sm:justify-end sm:m-0
             flex flex-1 mt-4
-          ">
+          "
+          >
             <Link href="/admin">
               <a aria-label="Admin panel">
-              <p className="text-sm font-semibold">Admins</p>
+                <p className="text-sm font-semibold">Admins</p>
               </a>
             </Link>
           </div>
